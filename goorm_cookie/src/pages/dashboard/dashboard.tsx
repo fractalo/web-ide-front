ce5a0a39949e72fe6ba3c657ae54fee5cd40a6f9
WhiteBearM <bhs9641@gmail.com>
2024-06-13 23:05:31 +0900

    실시간 코드 협업 세팅

---
+++ goorm_cookie/src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './dashboard.css'
+
+function dashboard() {
+  return (
+    <div className='ad'>
+      <h1>Dashboard</h1>
+      <p>Welcome to your Dashboard.</p>
+    </div>
+  );
+}
+
+export default dashboard
