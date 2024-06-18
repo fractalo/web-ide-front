36ef96b12d96295533395c5fbe63ce8928cae310
525tea <jene0028@gmail.com>
2024-06-18 21:49:19 +0900

    dashboard 소문자 수정

--- goorm_cookie/src/App.tsx
+++ goorm_cookie/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import C_Room from './pages/C_Room/C_Room';
-import Dashboard from './pages/Dashboard/Dashboard';
+import Dashboard from './pages/Dashboard/dashboard';
 import HomePage from './pages/HomePage';
 
 const App: React.FC = () => {
