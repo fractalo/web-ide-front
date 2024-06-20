a0b8bb4a6f98c05189ab143ca4eddc6c24bd31eb
WhiteBearM <bhs9641@gmail.com>
2024-06-20 13:21:12 +0900

    오류 사항 수정

--- goorm_cookie/src/main.tsx
+++ goorm_cookie/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // React 18에서는 'react-dom/client'를 사용
-import App from './App'; // 'App' 컴포넌트를 불러옴
+import App from './index'; // 'App' 컴포넌트를 불러옴
 
 // HTML 파일에서 'root'라는 id를 가진 요소를 가져옴
 const rootElement = document.getElementById('root');
