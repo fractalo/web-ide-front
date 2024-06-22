2b3d009d5da937a4d4fb6d53c6c0e632004a8753
525tea <jene0028@gmail.com>
2024-06-22 20:07:29 +0900

    중간

---
+++ goorm_cookie/src/pages/Projects/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import Header from '../../components/Header';
+import ChatComponent from '../../components/ChatComponent/ChatComponent';
+import ParticipantList from '../../components/ParticipantList';
+import './projects.css';
+import styled from 'styled-components';
+import userDummy from '../../dummy/user.dummy'
+
+
+const Dashboard: React.FC = () => {
+    const { roomId, lang } = useParams<{ roomId: string; lang: string }>();
+    const [usersOnline, setUsersOnline] = useState<{ id: string, name: string, isOnline: boolean }[]>([
+        { id: '1', name: 'Alice', isOnline: true },
+        { id: '2', name: 'Bob', isOnline: false }
+    ]);
+
+    return (
+        <StyledProjects id="projects">
+            <Header />
+            <div className="projects-grid">
+                <StyledProjectsGridLeft className="projects-grid-left">
+                    {roomId && lang }
+                </StyledProjectsGridLeft>
+
+                <div className="dashboard-grid-right chat-component-wrapper">
+                    <ParticipantList usersOnline={userDummy} />
+                    <ChatComponent />
+                </div>
+            </div>
+        </StyledProjects>
+    );
+};
+
+const StyledProjects = styled.div`
+    // background-color: red;
+    height : 100%;
+`
+
+const StyledProjectsGridLeft = styled.div`
+    margin-right : 30px;
+`
+
+export default Dashboard;
